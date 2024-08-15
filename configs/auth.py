9b2a1c01676a34253485c3545b772e05878e26ba
saman <saman.051y@gmail.com>
2024-08-15 11:20:52 +0330

    back auth

---
+++ configs/auth.py
@@ -0,0 +1,15 @@
+TOKEN='6950779316:AAEu7_lAtR-qcBQSSCr2LGtYoslhgj8E4S0' # @linkgp_adminBot
+BOT_USERNAME="linkgp_adminBot"
+DB_CONFIG={
+    'host':'localhost',
+    'port':'3333',
+    'user':'root',
+    'password':'root',
+    'database':'linkGP'}
+
+##############################################################
+# CHANNELS_USERNAME=['@linkgp','@test12test123s']
+CHANNELS_USERNAME=['@test12test123s']#todo: make it ok
+SUPPORT_USERNAME='@linkgp_admin'
+SUPPORT_ID=345490618
+ADMIN_ID_LIST=[340500740,1054820423]
