21054eed9fbcad04388f8aee8aa2fb3587746dff
saman <saman.051y@gmail.com>
2024-08-17 15:24:25 +0330

    final v2

--- configs/auth.py
+++ configs/auth.py
@@ -1,18 +1,18 @@
-# TOKEN='6950779316:AAEu7_lAtR-qcBQSSCr2LGtYoslhgj8E4S0' # @linkgp_adminBot
-TOKEN='380744213:AAEA1Y8Y4mDjUDzbuJQzxceZbaUWcr80iAU' # @Test051ybot
+TOKEN='6950779316:AAEu7_lAtR-qcBQSSCr2LGtYoslhgj8E4S0' # @linkgp_adminBot
+# TOKEN='380744213:AAEA1Y8Y4mDjUDzbuJQzxceZbaUWcr80iAU' # @Test051ybot
 BOT_USERNAME="linkgp_adminBot"
-# DB_CONFIG={
-#     'host':'172.86.110.111',
-#     'port':'3333',
-#     'user':'root',
-#     'password':'root',
-#     'database':'linkGP'}
 DB_CONFIG={
-    'host':'localhost',
+    'host':'172.86.110.111',
     'port':'3333',
     'user':'root',
     'password':'root',
     'database':'linkGP'}
+# DB_CONFIG={
+#     'host':'localhost',
+#     'port':'3333',
+#     'user':'root',
+#     'password':'root',
+#     'database':'linkGP'}
 
 ##############################################################
 # CHANNELS_USERNAME=['@linkgp']
