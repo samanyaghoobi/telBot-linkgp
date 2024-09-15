faafb82ba96b8d1b6a07062c5f6b9ee3e8930a11
saman <saman.051y@gmail.com>
2024-09-15 13:55:34 +0330

    auth

--- configs/auth.py
+++ configs/auth.py
@@ -1,7 +1,7 @@
-# TOKEN='6950779316:AAEu7_lAtR-qcBQSSCr2LGtYoslhgj8E4S0' # @linkgp_adminBot
-# CHANNELS_USERNAME=['@test12test123s']#todo: make it ok
+TOKEN='6950779316:AAEu7_lAtR-qcBQSSCr2LGtYoslhgj8E4S0' # @linkgp_adminBot
 CHANNELS_USERNAME=['@linkgp']
-TOKEN='380744213:AAEA1Y8Y4mDjUDzbuJQzxceZbaUWcr80iAU' # @Test051ybot
+#TOKEN='380744213:AAEA1Y8Y4mDjUDzbuJQzxceZbaUWcr80iAU' # @Test051ybot
+# CHANNELS_USERNAME=['@test12test123s']#todo: make it ok
 BOT_USERNAME="linkgp_adminBot"
 DB_CONFIG={
     'host': '166.88.209.208',
