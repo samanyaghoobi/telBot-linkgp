e5b5ef86553184b858d3d58b01634b64c07020fe
saman <saman.051y@gmail.com>
2024-11-05 16:31:06 +0330

    fix card number

--- message_and_text/bot_message_functions.py
+++ message_and_text/bot_message_functions.py
@@ -32,9 +32,9 @@ def make_change_score_text(score:int,convert_able:int,value:int):
 
 ################################3
 def get_cart_info()->str:
-     CART_NUMBER=db_botSetting_getValue(name='CART_NUMBER')[0]
-     CART_NAME=db_botSetting_getValue(name='CART_NAME')[0]
-     CART_BANK=db_botSetting_getValue(name='CART_BANK')[0]
+     CART_NUMBER=db_botSetting_getValue(name='CART_NUMBER')
+     CART_NAME=db_botSetting_getValue(name='CART_NAME')
+     CART_BANK=db_botSetting_getValue(name='CART_BANK')
      cart_info_text=f"""💳 شماره کارت : <code>{CART_NUMBER}</code>
      👤 مالک کارت : {CART_NAME} 
      🏦 بانک : {CART_BANK}"""
